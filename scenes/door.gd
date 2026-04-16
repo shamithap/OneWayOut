4d3569fc034a64f2ba0771be6e73ebb8c9bfb01a
abbyhayd <abigayle.doinog@gmail.com>
2026-04-15 17:17:56 -0700

    fixed door trigger

--- scenes/door.gd
+++ scenes/door.gd
@@ -2,8 +2,9 @@ extends Area2D
 
 @onready var hint = $"../../Hint"
 
-func _on_body_entered(body: Node2D) -> void:
+
+func _on_door_body_entered(_body: Node2D) -> void:
 	hint.visible(true)
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_door_body_exited(_body: Node2D) -> void:
 	hint.visible(false)
