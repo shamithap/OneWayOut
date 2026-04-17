5304f9595a9b763263660327c13177b2e6c8ac6c
Shamitha Pichika <spichika@seattleu.edu>
2026-04-17 11:25:15 -0700

    added player animation and movement logic

---
+++ scenes/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var speed := 120.0
+@onready var anim = $AnimatedSprite2D
+
+var last_direction = "down"
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+
+	velocity = direction * speed
+	move_and_slide()
+
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				last_direction = "right"
+			else:
+				last_direction = "left"
+		else:
+			if direction.y > 0:
+				last_direction = "down"
+			else:
+				last_direction = "up"
+
+		anim.play("walk_" + last_direction)
+	else:
+		anim.play("idle_" + last_direction)
