0fa34669c0244d764a43366eb41dd13dda3293da
Shamitha Pichika <spichika@seattleu.edu>
2026-04-17 11:47:02 -0700

    fixed hint visibility

--- scenes/door.gd
+++ scenes/door.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var hint = $"../Hint"
 
 func _on_door_body_entered(_body: Node2D) -> void:
-	hint.visible(true)
+	hint.visible = true
 
 func _on_door_body_exited(_body: Node2D) -> void:
-	hint.visible(false)
+	hint.visible = false
