e64aa277f3bbefff69fa91d2bc47cafc9f7b2d95
Shamitha Pichika <spichika@seattleu.edu>
2026-04-17 13:54:13 -0700

    added interaction with door 1

--- scenes/door.gd
+++ scenes/door.gd
@@ -2,8 +2,19 @@ extends Node2D
 
 @onready var hint = $"../Hint"
 
+#helps keep track when player is nearby
+var player_in_range = false 
+
+
 func _on_door_body_entered(_body: Node2D) -> void:
 	hint.visible = true
+	player_in_range = true 
 
 func _on_door_body_exited(_body: Node2D) -> void:
 	hint.visible = false
+	player_in_range = false
+
+func _process(_delta):
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		Global.spawn_location = "door1" 
+		get_tree().change_scene_to_file("res://scenes/room1.tscn")
