cd018d5181b1a560529cd951ef626a6a1428230f
abbyhayd <abigayle.doinog@gmail.com>
2026-04-17 19:59:50 -0700

    created scene_dict in navigation_manager

--- scripts/global/navigation_manager.gd
+++ scripts/global/navigation_manager.gd
@@ -1,8 +1,10 @@
 extends Node
 
-const scene_main = preload("res://scenes/rooms/main.tscn")
-const scene_room1 = preload("res://scenes/rooms/room1.tscn")
-const scene_room3 = preload("res://scenes/rooms/room3.tscn")
+const scene_dict = {
+	"main" : preload("res://scenes/rooms/main.tscn"),
+	"room1": preload("res://scenes/rooms/room1.tscn"),
+	 "room3" : preload("res://scenes/rooms/room3.tscn")
+}
 
 signal on_trigger_player_spawn
 
@@ -14,13 +16,7 @@ var player : Player = null
 #input the room that you want to go to and the tag of the 
 #door the player should be spawning at
 func go_to_level(destination_room_name, destination_door_tag):
-	match destination_room_name:
-		"main":
-			scene_to_load = scene_main
-		"room1":
-			scene_to_load = scene_room1
-		"room3":
-			scene_to_load = scene_room3
+	scene_to_load = scene_dict[destination_room_name]
 	
 	if scene_to_load != null:
 		spawn_door_tag = destination_door_tag
