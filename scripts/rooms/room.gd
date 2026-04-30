11843038e74c2e840575efaf800ef7e1786513ef
abbyhayd <abigayle.doinog@gmail.com>
2026-04-29 17:52:09 -0700

    fixed main position and removed unnecessary function from room.gd

--- scripts/rooms/room.gd
+++ scripts/rooms/room.gd
@@ -23,11 +23,3 @@ func _on_level_spawn(destination_tag : String):
 	var door_path = "Doors/Door_" + destination_tag
 	var door = get_node(door_path) as Door
 	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
-
-
-func _on_trigger_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_trigger_area_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
