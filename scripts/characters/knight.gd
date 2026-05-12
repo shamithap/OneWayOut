d599130d423760092858b76ecf9adf3e8117fedb
abbyhayd <abigayle.doinog@gmail.com>
2026-05-12 13:02:01 -0700

    created base minimap

--- scripts/characters/knight.gd
+++ scripts/characters/knight.gd
@@ -5,11 +5,9 @@ extends StaticBody2D
 @onready var indicator_bubble := $Bubbles/IndicatorBubble
 var player_in_range : bool = false
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
