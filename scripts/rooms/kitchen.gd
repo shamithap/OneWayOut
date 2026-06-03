11a82257e86610d68d2ac297213aa27de42f237c
abbyhayd <abigayle.doinog@gmail.com>
2026-06-03 13:23:43 -0700

    fixed kitchen chest

--- scripts/rooms/kitchen.gd
+++ scripts/rooms/kitchen.gd
@@ -17,8 +17,7 @@ func _ready():
 	if NavigationManager.spawn_door_tag != null :
 		_on_level_spawn(NavigationManager.spawn_door_tag)
 
-	if Global.kitchen_complete:
-		already_won()
+	already_won()
 
 #gets the spawn marker from the door and send that info to the player
 #through a signal so it spawns at the correct place
@@ -26,7 +25,6 @@ func _on_level_spawn(destination_tag : String):
 	var door_path = "Doors/Door_" + destination_tag
 	var door = get_node(door_path) as Door
 	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
-	
 	
 func already_won():
 	if not Global.kitchen_item_collected:
