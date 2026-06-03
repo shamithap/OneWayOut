d48124b9408d1b887a168b8d38a4e541e07caada
CJgracee <cjiang@seattleu.edu>
2026-06-02 18:02:26 -0700

    Set dining room to use custom script

--- scripts/rooms/diningroom.gd
+++ scripts/rooms/diningroom.gd
@@ -1,11 +1,80 @@
-extends Node
+extends "res://scripts/rooms/room.gd"
 
+@onready var dining_hint = get_node_or_null("CanvasLayer/DiningHint")
+@onready var puzzle_panel = get_node_or_null("CanvasLayer/DinnerPuzzlePanel")
+@onready var result_label = get_node_or_null("CanvasLayer/DinnerPuzzlePanel/ResultLabel")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var in_table_range : bool = false
+var dinner_puzzle_solved : bool = false
 
+func _ready():
+	super._ready()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if dining_hint != null:
+		dining_hint.visible = false
+
+	if puzzle_panel != null:
+		puzzle_panel.visible = false
+
+func _process(_delta: float) -> void:
+	if in_table_range and Input.is_action_just_pressed("interact") and not dinner_puzzle_solved:
+		open_dinner_puzzle()
+
+func open_dinner_puzzle():
+	if puzzle_panel != null:
+		puzzle_panel.visible = true
+
+	if dining_hint != null:
+		dining_hint.visible = false
+
+	if NavigationManager.player != null:
+		NavigationManager.player.can_move = false
+
+	if result_label != null:
+		result_label.text = ""
+
+func close_dinner_puzzle():
+	if puzzle_panel != null:
+		puzzle_panel.visible = false
+
+	if NavigationManager.player != null:
+		NavigationManager.player.can_move = true
+
+	if dining_hint != null and in_table_range and not dinner_puzzle_solved:
+		dining_hint.visible = true
+
+func choose_wrong_answer():
+	if result_label != null:
+		result_label.text = "That belongs to the living."
+
+func choose_correct_answer():
+	dinner_puzzle_solved = true
+
+	if result_label != null:
+		result_label.text = "Correct! Dinner was never meant to be eaten."
+
+	if NavigationManager.player != null:
+		NavigationManager.player.can_move = true
+
+	print("Dining room puzzle solved!")
+
+func _on_table_area_body_entered(body: Node2D) -> void:
+	if body is Player and not dinner_puzzle_solved:
+		in_table_range = true
+
+		if dining_hint != null:
+			dining_hint.visible = true
+			dining_hint.text = "Press E to inspect the dinner"
+
+func _on_table_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		in_table_range = false
+
+		if dining_hint != null:
+			dining_hint.visible = false
+
+		if puzzle_panel != null:
+			puzzle_panel.visible = false
+
+		if NavigationManager.player != null:
+			NavigationManager.player.can_move = true
