dbf9ed7922f5fba8a8ec619cb6cf4fe7f08b94c8
naikhri23 <vc567hn@gmail.com>
2026-06-03 11:30:36 -0700

    Global conflict resolved

--- scripts/global/Global.gd
+++ scripts/global/Global.gd
@@ -16,10 +16,6 @@ var ballroom_item_collected = false
 var ballroom_complete = false
 var diningroom_item_collected = false
 var diningroom_complete = false
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 var recroom_complete = false
 var player_health = 3
 var max_health = 3
