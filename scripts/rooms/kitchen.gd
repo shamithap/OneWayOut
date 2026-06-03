c37f465e1fbbd54207e270e85207a69828d986c5
naikhri23 <vc567hn@gmail.com>
2026-06-03 12:33:08 -0700

    Chest in kitchen

--- scripts/rooms/kitchen.gd
+++ scripts/rooms/kitchen.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var player_packed_scene = preload("res://scenes/player.tscn")
+@onready var chest := $RewardChest/Sprite2D
+@onready var chest_marker := $RewardChest/Marker2D
 var player : Player = null
 
 #makes sure the player is in the scene
@@ -15,9 +17,20 @@ func _ready():
 	if NavigationManager.spawn_door_tag != null :
 		_on_level_spawn(NavigationManager.spawn_door_tag)
 
+	if Global.kitchen_complete:
+		already_won()
+
 #gets the spawn marker from the door and send that info to the player
-#through a signal so it spawns at teh correct place
+#through a signal so it spawns at the correct place
 func _on_level_spawn(destination_tag : String):
 	var door_path = "Doors/Door_" + destination_tag
 	var door = get_node(door_path) as Door
 	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
+	Global.kitchen_complete = true
+	
+	
+func already_won():
+	chest.animation = "open"
+	if not Global.kitchen_item_collected:
+		ItemManager.get_item(chest_marker.global_position)
+		Global.kitchen_item_collected = true
