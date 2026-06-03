0501dc5cbff2f22d519454957ccb43209f126a9f
CJgracee <cjiang@seattleu.edu>
2026-06-03 16:26:06 -0700

    fixed armory

--- scripts/characters/wizard.gd
+++ scripts/characters/wizard.gd
@@ -1,25 +1,20 @@
 extends StaticBody2D
 
-@onready var hint : Label = $"../CanvasLayer/Hint"
+@onready var hint : Label = $Bubbles/ChatBubble/Label
 @onready var chat_bubble:= $Bubbles/ChatBubble
 @onready var indicator_bubble := $Bubbles/IndicatorBubble
-@onready var sound := $AudioStreamPlayer2D
 var player_in_range : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	
-func _process(_delta: float) -> void:
-	if player_in_range and Input.is_action_just_pressed("interact"):
-		get_tree().change_scene_to_file("res://scenes/rooms/armory/rps_game.tscn")	
 
 func _on_trigger_area_body_entered(body: Node2D) -> void:
 	if body is Player:
 		switch_bubbles(true)
 		hint.visible = true
 		player_in_range = true
-		sound.play()
+		hint.text = "Hey, I'm using it! Get out!!!"
 
 func _on_trigger_area_body_exited(body: Node2D) -> void:
 	if body is Player:
