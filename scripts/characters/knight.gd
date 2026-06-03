8229f291423a4429233b994a4f6dfe540a4bc6a3
naikhri23 <vc567hn@gmail.com>
2026-06-03 13:06:55 -0700

    Bathroom Finished

--- scripts/characters/knight.gd
+++ scripts/characters/knight.gd
@@ -1,27 +1,20 @@
 extends StaticBody2D
 
-@onready var hint : Label = $"../CanvasLayer/Hint"
+@onready var hint : Label = $Bubbles/ChatBubble/Label
 @onready var chat_bubble:= $Bubbles/ChatBubble
 @onready var indicator_bubble := $Bubbles/IndicatorBubble
-@onready var sound := $AudioStreamPlayer2D
 var player_in_range : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if player_in_range and Input.is_action_just_pressed("interact"):
-		get_tree().change_scene_to_file("res://scenes/rooms/armory/rps_game.tscn")
-
 func _on_trigger_area_body_entered(body: Node2D) -> void:
 	if body is Player:
 		switch_bubbles(true)
 		hint.visible = true
 		player_in_range = true
-		hint.text = "Press E to play"
-		sound.play()
+		hint.text = "Hey! Get out!!!"
 
 func _on_trigger_area_body_exited(body: Node2D) -> void:
 	if body is Player:
--- scripts/rooms/kitchen.gd
+++ scripts/rooms/kitchen.gd
@@ -26,7 +26,6 @@ func _on_level_spawn(destination_tag : String):
 	var door_path = "Doors/Door_" + destination_tag
 	var door = get_node(door_path) as Door
 	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
-	Global.kitchen_complete = true
 	
 	
 func already_won():
