69eb74f06fa170a6d22cf879109a8b9584ba29b0
CJgracee <cjiang@seattleu.edu>
2026-06-03 00:03:33 -0700

    Added clock sound effects to living room puzzle

--- scripts/rooms/clock_puzzle.gd
+++ scripts/rooms/clock_puzzle.gd
@@ -6,6 +6,8 @@ extends Node2D
 @onready var minute_input = $PuzzlePanel/MinuteInput
 @onready var check_button = $PuzzlePanel/CheckButton
 @onready var result_label = $PuzzlePanel/ResultLabel
+@onready var tick_sound = $TickSound
+@onready var chime_sound = $ChimeSound
 
 var player_near = false
 var puzzle_solved = false
@@ -14,6 +16,10 @@ func _ready():
 	hint_label.visible = false
 	puzzle_panel.visible = false
 	result_label.text = ""
+	
+	#make sure both sounds are stopped when the scene starts
+	tick_sound.stop()
+	chime_sound.stop()
 
 	check_button.pressed.connect(_on_check_button_pressed)
 
@@ -30,11 +36,19 @@ func _on_interact_area_body_entered(body):
 		player_near = true
 		if not puzzle_solved:
 			hint_label.visible = true
+			
+			#play ticking sound when player gets close to the clock
+			# The "if not playing" check prevents the sound from restarting repeatedly
+			if not tick_sound.playing:
+				tick_sound.play()
 
 func _on_interact_area_body_exited(body):
 	if body is Player:
 		player_near = false
 		hint_label.visible = false
+		# stop ticking sound when player leaves the clock area
+		if tick_sound.playing:
+			tick_sound.stop()
 
 func _on_check_button_pressed():
 	var hour = hour_input.text.strip_edges()
@@ -47,6 +61,13 @@ func _on_check_button_pressed():
 		minute_input.visible = false
 		check_button.visible = false
 		hint_label.visible = false
+		
+		#stop ticking sound when the puzzle is solved
+		if tick_sound.playing:
+			tick_sound.stop()
+		
+		#play chime sound as a success sound
+		chime_sound.play()
 		
 		result_label.text = "Correct! \nA hidden note:\n\"Dinner is waiting in the Dining Room.\"\nPress E to close."
 	else:
